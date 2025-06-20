fa470802b1341d155f7eb93dbe96a6119c5ef974
Flash0ver <38893694+Flash0ver@users.noreply.github.com>
2025-06-20 20:53:31 +0200

    feat: add Sentry

--- src/MauiProgram.cs
+++ src/MauiProgram.cs
@@ -13,6 +13,10 @@ public static class MauiProgram
 			{
 				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
 				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+			})
+			.UseSentry(options =>
+			{
+				options.Dsn = "<Dsn>";
 			});
 
 #if DEBUG
